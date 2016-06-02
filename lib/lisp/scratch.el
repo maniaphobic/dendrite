c6eea56acbf85b8dbe93df31653e7ab2966057e7
mANIApHOBIC <git@maniaphobic.org>
2016-06-01 19:33:10 -0700

    Checkpoint commit
    This commit barely works

--- lib/lisp/scratch.el
+++ lib/lisp/scratch.el
@@ -1,22 +1,57 @@
 (require 'json)
 
-(let ((default-directory "~/Development/github/maniaphobic/dendrite/chef"))
-  (rvm-use "2.2" "(default)")
-  (start-process "irb" "irb" "irb" "--noprompt")
-  )
-;DISABLED;(delete-process "irb")
+(setq chef-base-directory
+      (expand-file-name "~/Development/github/maniaphobic/dendrite/chef"))
 
-(process-send-string "irb" "
+(defun find-chef-config (dirs file)
+  (if (eq dirs nil) nil
+    (let ((path (concat (car dirs) "/" file)))
+      (if (file-exists-p path) path (find-chef-config (cdr dirs) file)))))
+
+(defun load-chef-config ()
+  (process-send-string
+   "irb"
+   (format "
 require 'chef/config'
 require 'chef/node'
-Chef::Config.from_file('./.chef/knife.rb')
+Chef::Config.from_file('%s')
+\n" (find-chef-config
+     (list default-directory (concat default-directory "/.chef") "~/.chef")
+     "knife.rb"))))
+
+(defun launch-irb (version gemset)
+    (rvm-use version gemset)
+    (unless (get-buffer-process "irb")
+      (start-process "irb" "irb" "irb" "--noprompt")))
+
+(defun chef/get (object)
+  (cond ((eq object :nodes)
+	 (process-send-string "irb" "Chef::Node.list.to_json\n"))
+	 nil))
+
+(defun chef/explore (base-directory)
+  (let ((default-directory chef-base-directory))
+    (launch-irb "2.2" "(default)")
+    (load-chef-config)
+    (chef/get :nodes)
+    )
+  )
+
+(defun needs-more-work ()
+  (let ((start (process-mark (get-process "irb")))
+	end)
+    (process-send-string "irb" "
+Chef::Node.list.to_json
 \n")
+    (setq end (process-mark (get-process "irb")))
+    (save-excursion
+      (switch-to-buffer "irb")
+      (buffer-substring start end)))
+  (json-read-from-string (buffer-substring start end))
+  )
 
-(process-send-string "irb" "
-Chef::Node.list
-\n"
-)
+(defun stop-irb () (delete-process "irb"))
 
-(process-mark (get-process "irb"))
+; Application entry point
 
-
+(chef/explore chef-base-directory)
